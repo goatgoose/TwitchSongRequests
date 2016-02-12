ff4edddd0373e035c50df9a625e35ef319020699
Sam Clark <sam.clark1142@gmail.com>
2016-02-12 16:39:47 -0500

    update?

---
+++ m/routes/api.js
@@ -0,0 +1,18 @@
+/**
+ * Created by goatgoose on 1/16/16.
+ */
+var express = require('express');
+var router = express.Router();
+
+/*
+ * GET userlist.
+ */
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('users');
+    collection.find({},{},function(e,docs){
+        res.json(docs);
+    });
+});
+
+module.exports = router;
